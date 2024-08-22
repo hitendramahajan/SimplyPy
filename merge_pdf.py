49ff9b9f5a8b9757c88ef6fbb7522c09cca1dc17
hitendramahajan <49238730+hitendramahajan@users.noreply.github.com>
2024-08-22 13:43:42 +0530

    Create merge_pdf.py

---
+++ merge_pdf.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfFileMerger
+
+def merge_pdf():
+    merger = PdfFileMerger()
+    print(merger.id_count)  # Note: This line will raise an AttributeError because id_count does not exist
+    merger.close()
+
+if __name__ == "__main__":
+    merge_pdf()
